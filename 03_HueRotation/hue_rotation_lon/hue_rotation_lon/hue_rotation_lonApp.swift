fe05da858a225bb628d9ce5aca158cc01b521973
MacBookAir <alonzo.choque@icloud.com>
2024-04-20 17:15:37 -0400

    MacBookAir2017 20240420 17:14 Create and End 03_HueRotation Project

---
+++ 03_HueRotation/hue_rotation_lon/hue_rotation_lon/hue_rotation_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  hue_rotation_lonApp.swift
+//  hue_rotation_lon
+//
+//  Created by Rene Alonzo Choque Saire on 20/4/24.
+//
+
+import SwiftUI
+
+@main
+struct hue_rotation_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
