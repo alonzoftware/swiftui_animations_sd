516fb20c078009b2df09ca468f677ead885c2ec0
MacBookAir2019 <alonzo.choque@icloud.com>
2024-04-23 20:37:11 -0400

    MacBookAir2019 20240423 20:36 Create and End 06_SpriteKitAnimation Project

---
+++ 06_SpriteKitAnimation/spritekit_animation_lon/spritekit_animation_lon/spritekit_animation_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  spritekit_animation_lonApp.swift
+//  spritekit_animation_lon
+//
+//  Created by MacBookAir on 23/4/24.
+//
+
+import SwiftUI
+
+@main
+struct spritekit_animation_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
