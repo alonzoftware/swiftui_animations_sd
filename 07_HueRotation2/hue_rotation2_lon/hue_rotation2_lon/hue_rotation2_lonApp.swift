32dc7c8b7b30fcb80e743e034461a38a01b3e540
MacBookAir2019 <alonzo.choque@icloud.com>
2024-04-25 21:00:00 -0400

    MacBookAir2019 20240425 08:59 Create and End 07 and 08 Projects

---
+++ 07_HueRotation2/hue_rotation2_lon/hue_rotation2_lon/hue_rotation2_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  hue_rotation2_lonApp.swift
+//  hue_rotation2_lon
+//
+//  Created by MacBookAir on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct hue_rotation2_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 08_PresentDismiss_AnimateTransitions/present_dismiss_lon/present_dismiss_lon/present_dismiss_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  present_dismiss_lonApp.swift
+//  present_dismiss_lon
+//
+//  Created by MacBookAir on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct present_dismiss_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
