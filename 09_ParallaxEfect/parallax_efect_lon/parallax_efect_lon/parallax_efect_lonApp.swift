6f868b6a0289aa8d7dbb952aa396fcc77594be03
MacBookAir2019 <alonzo.choque@icloud.com>
2024-04-26 18:45:31 -0400

    MacBookAir2019 20240426 18:44 Create and Finish Parallax Effect Project

---
+++ 09_ParallaxEfect/parallax_efect_lon/parallax_efect_lon/parallax_efect_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  parallax_efect_lonApp.swift
+//  parallax_efect_lon
+//
+//  Created by MacBookAir on 26/4/24.
+//
+
+import SwiftUI
+
+@main
+struct parallax_efect_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
