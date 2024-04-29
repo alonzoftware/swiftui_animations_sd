9abc0b0520d3680a74f42fe248aefde1c61d5d60
MacBookAir <alonzo.choque@icloud.com>
2024-04-29 11:05:22 -0400

    MacBookAir2017 20240429 11:02 Create and Finish Swinging Animation

---
+++ 11_SwingingAnimation/swinging_animation_lon/swinging_animation_lon/swinging_animation_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  swinging_animation_lonApp.swift
+//  swinging_animation_lon
+//
+//  Created by Rene Alonzo Choque Saire on 29/4/24.
+//
+
+import SwiftUI
+
+@main
+struct swinging_animation_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
