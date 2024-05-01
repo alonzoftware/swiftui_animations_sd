efb77b7c8d7d5fc79d50f760354c5285d124634e
MacBookAir2019 <alonzo.choque@icloud.com>
2024-04-30 20:29:45 -0400

    MacBookAir2019 20240430 20:30 Create and Finish Twinkling Stars Project

---
+++ 12_TwinklingStars/twinkling_stars_lon/twinkling_stars_lon/twinkling_stars_lonApp.swift
@@ -0,0 +1,17 @@
+//
+//  twinkling_stars_lonApp.swift
+//  twinkling_stars_lon
+//
+//  Created by MacBookAir on 30/4/24.
+//
+
+import SwiftUI
+
+@main
+struct twinkling_stars_lonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
